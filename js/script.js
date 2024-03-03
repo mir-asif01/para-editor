0e380f9538cb800f283268cbf96a9cab8095359b
mir-asif01 <asifbsb2002@gmail.com>
2024-03-03 20:24:26 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+const input = document.getElementById("inputField")
+const boldBtn = document.getElementById("bold")
+const italicBtn = document.getElementById("italic")
+const underlineBtn = document.getElementById("underline")
+const color = document.getElementById('color')
+const bgColor = document.getElementById('bgColor')
+
+let boldActive = false
+let italicActive = false
+let underlineActive = false
+
+
+boldBtn.addEventListener('click', () => {
+
+    boldActive = !boldActive
+    if (boldActive === true) {
+        boldBtn.classList.add('active')
+    }
+    if (boldActive === false) {
+        boldBtn.classList.remove('active')
+    }
+})
+italicBtn.addEventListener('click', () => {
+    italicActive = !italicActive
+    if (italicActive === true) {
+        italicBtn.classList.add('active')
+    }
+    if (italicActive === false) {
+        italicBtn.classList.remove('active')
+    }
+})
+underlineBtn.addEventListener('click', () => {
+    underlineActive = !underlineActive
+    if (underlineActive === true) {
+        underlineBtn.classList.add('active')
+    }
+    if (underlineActive === false) {
+        underlineBtn.classList.remove('active')
+    }
+})
+
+function changeStyle() {
+    if (boldActive === true) {
+        input.classList.add('bold')
+    }
+    if (boldActive === false) {
+        input.classList.remove('bold')
+    }
+    if (italicActive === true) {
+        input.classList.add('italic')
+    }
+    if (italicActive === false) {
+        input.classList.remove('italic')
+    }
+    if (underlineActive === true) {
+        input.classList.add('underline')
+    }
+    if (underlineActive === false) {
+        input.classList.remove('underline')
+    }
+    let colorValue = color.value
+    input.style.color = colorValue
+    let bgColorValue = bgColor.value
+    console.log(typeof (bgColor.value));
+    input.style.backgroundColor = bgColorValue
+}
+
+document.getElementById('changeStyle').addEventListener('click', changeStyle)
